53274abb7ca4a9f64486d434fc5d32f132c69dec
Praveen778 <117338264+Praveen778@users.noreply.github.com>
2024-11-23 12:19:25 +0530

    firstcommit

---
+++ htmlcssproject/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+import './App.css'
+import Gitexample from './Gitexample'
+
+function App() {
+  
+
+  return (
+   
+    <div>
+    <Gitexample />
+    </div>
+     
+       
+   
+  )
+}
+
+export default App
